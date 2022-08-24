bab5942ae25f7570dabe222ddd0d9fe1c69c7365
user9527 <631199080@qq.com>
2022-08-24 17:28:25 +0800

    second commit

---
+++ src/main/java/com/user/First.java
@@ -0,0 +1,12 @@
+package com.user;
+
+/**
+ * @author yangtao
+ * @version 1.0
+ * @date 2022/8/24 17:24
+ */
+public class First {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
